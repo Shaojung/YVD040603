0ab32c93c884b3c6ab50de92028b1e61655054f0
Shaojung Lu <shaojung@gmail.com>
2017-04-17 10:27:23 +0800

    put xml data into listview

--- app/src/main/java/com/example/yvtc/yvd040603/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvd040603/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.xml.sax.InputSource;
@@ -27,11 +29,14 @@ public class MainActivity extends AppCompatActivity {
     InputStream inputStream;
     MyDataHandler dataHandler;
     TextView tv;
+    ListView lv;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) findViewById(R.id.textView);
+        // tv = (TextView) findViewById(R.id.textView);
+        lv = (ListView) findViewById(R.id.listView);
         dataHandler = new MyDataHandler();
         new Thread(){
             @Override
@@ -62,7 +67,9 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            tv.setText(sb.toString());
+                            // tv.setText(sb.toString());
+                            adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, dataHandler.titles);
+                            lv.setAdapter(adapter);
                         }
                     });
 
