95f7907df46257d67870585676eecab51df8a64e
Shaojung Lu <shaojung@gmail.com>
2017-04-17 11:34:00 +0800

    put xml data into listview

--- app/src/main/java/com/example/yvtc/yvd040603/DetailActivity.java
+++ app/src/main/java/com/example/yvtc/yvd040603/DetailActivity.java
@@ -2,13 +2,18 @@ package com.example.yvtc.yvd040603;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class DetailActivity extends AppCompatActivity {
-
+    WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        wv = (WebView) findViewById(R.id.webView);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl(getIntent().getStringExtra("link"));
     }
 }
